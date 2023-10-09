62d558809b16260aa037a05332e561685e541a1a
wataru_nagatsuka <wataru.nagatsuka@bric.jp>
2023-10-09 23:10:51 +0900

    add: task_listページ表示まで

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic.list import ListView
+
+from todo_app.models import Task
 
 
-def taskList(request):
-    return HttpResponse('<h1>Hello Django</h1>')
+class TaskList(ListView):
+    """TaskList
+    https://docs.djangoproject.com/en/4.2/ref/class-based-views/generic-display/#listview
+    """
+    model = Task

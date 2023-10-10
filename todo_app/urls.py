66ce21a9ba49ef4aeb880661c8b6d85ec9d7259f
wataru_nagatsuka <wataru.nagatsuka@bric.jp>
2023-10-10 12:57:51 +0900

    add: 詳細ページ、作成ページ

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import TaskList
+from .views import TaskCreate, TaskList, TaskDetail
 
 
 urlpatterns = [
-    path('', TaskList.as_view()),
+    path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>', TaskDetail.as_view(), name='task'),  # PrimaryKeyの動的URL
+    path('create-task', TaskCreate.as_view(), name='create-task'),  # PrimaryKeyの動的URL
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 
 from todo_app.models import Task
 
@@ -11,3 +14,15 @@ class TaskList(ListView):
     """
     model = Task
     context_object_name = 'tasks'  # templateでの変数名を変える
+
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'  # ['user', 'title'...]と同じ
+    # 作成後のリダイレクト
+    success_url = reverse_lazy('tasks')  # urls.pyでのページ名と対応

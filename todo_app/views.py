1cc767c52b47da788557f84664614c2f7071102d
nagawanage <barclays0pl0@gmail.com>
2023-10-14 12:11:23 +0900

    add: アカウント新規登録画面追加

--- todo_app/views.py
+++ todo_app/views.py
@@ -5,9 +5,11 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 from todo_app.models import Task
 
@@ -67,3 +69,17 @@ class TaskListLoginView(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('tasks')
+
+
+class RegisterTodoApp(FormView):
+    """アカウント新規登録"""
+    template_name = 'todo_app/register.html'
+    form_class = UserCreationForm  # ユーザー登録formに設定
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form: Any) -> HttpResponse:
+        # アカウント登録
+        user = form.save()
+        if user:
+            login(self.request, user)
+        return super().form_valid(form)

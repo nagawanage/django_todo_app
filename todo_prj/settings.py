6ac6b4fc63464be430c2b5c3ad05720d4a6aa511
nagawanage <barclays0pl0@gmail.com>
2023-10-14 22:38:11 +0900

    update: タスク一覧画面デザイン調整

--- todo_prj/settings.py
+++ todo_prj/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todo_app'  # 追加
+    'todo_app',  # 追加
+    'fontawesomefree',
 ]
 
 MIDDLEWARE = [
